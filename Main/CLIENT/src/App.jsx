82bf7e788498f6bc23966be3e4475fc54deb5dc7
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2024-03-02 19:33:36 +0530

    adding-footer-to-main-project

--- Main/CLIENT/src/App.jsx
+++ Main/CLIENT/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import {Foooter} from './components/Foooter'
 
 function App() {
 
   return (
     <>
       <h1>🤚🏻</h1>
+      <Foooter/>
     </>
   )
 }
