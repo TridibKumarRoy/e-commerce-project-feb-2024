b03e6385b4023cb1de46697736ffdebba8e16488
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2024-03-03 20:00:26 +0530

    setup

--- Main/CLIENT/src/App.jsx
+++ Main/CLIENT/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import {Foooter} from './components/Foooter'
+import {Navbar} from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1>🤚🏻</h1>
+      <Navbar/>
       <Foooter/>
     </>
   )
