dd86803e3d7b5782dbebb28c88ba20c75bab7757
Md ImtazUddin Sk <imtiazjeet960@gmail.com>
2024-03-20 20:32:38 +0530

    0

--- newsets/client/src/components/layouts/Home/Home.jsx
+++ newsets/client/src/components/layouts/Home/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import "./home.css"
+// import {cgMouse} from "react-icons/all";
+import Product from "./Product.jsx"
+
 
 export const Home = () => {
     return (<>
         <main>
-            dhdh
+            <Fragment>
+                <div className='banner'>
+                    <p>Welcome to Bhoom</p>
+                    <h1>Find amazing accessories below</h1>
+
+                    <a href="#container">
+                        <button>
+                            Scroll <cgMouse/>
+                        </button>
+                    </a>
+                </div>
+
+                <h2 className='homeHeading'>Featured Accessories</h2>
+
+                <div className='container' id='container'>
+
+                    <Product product={product}/>
+                </div>
+
+
+            </Fragment>
         </main>
     </>)
 }
