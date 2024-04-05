35aaf4963e2bd14e187a0caa062248c5016445c6
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2024-04-05 18:18:24 +0530

    reduxremoved

--- newsets/client/src/components/Home/Home.jsx
+++ newsets/client/src/components/Home/Home.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import "./home.css";
-// import {cgMouse} from "react-icons/all";
 import Product from "./Product";
 import Metadata from "../layouts/Metadata";
-import { clearErrors, getProduct } from "../../actions/productAction";
-import { useSelector, useDispatch } from "react-redux";
 import Slider from './Slider'; 
 
 const product = {
@@ -15,16 +12,17 @@ const product = {
 }
 // const title = 'home page';
 export const Home = () => {
-    const dispatch = useDispatch();
-    const { loading, error, products } = useSelector((state) => state.products);
 
-    useEffect(() => {
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
-        dispatch(getProduct());
-    }, [dispatch, error, alert]);
+    // const dispatch = useDispatch();
+    // const { loading, error, products } = useSelector((state) => state.products);
+
+    // useEffect(() => {
+    //     if (error) {
+    //         alert.error(error);
+    //         dispatch(clearErrors());
+    //     }
+    //     dispatch(getProduct());
+    // }, [dispatch, error, alert]);
 
 
     const images = [
@@ -38,15 +36,6 @@ export const Home = () => {
             <Metadata title='home page' />
 
             <Slider images={images} />
-            {/* <div className="banner"> */}
-                {/* <p>Welcome to Bhoom</p>
-                <h1>Find amazing accessories below</h1>
-
-                <a href="#mainContainer">
-                    <button>Scroll</button>
-                </a> */}
-
-            {/* </div> */}
 
             <div className="mainContainer" id="mainContainer">
 
