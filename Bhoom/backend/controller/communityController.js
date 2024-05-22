3c90dc9a9e99716b742c46262d424f92c5e81e0b
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2024-05-22 22:58:43 +0530

    community controller

---
+++ Bhoom/backend/controller/communityController.js
@@ -0,0 +1,71 @@
+const catchAsyncError = require("../middleware/catchAsyncError");
+const ErrorHandler = require("../utils/errorHandler");
+const Community = require("../models/contactModel");
+
+
+exports.newPost = catchAsyncError(
+    async (req, res, next) => {
+        const {
+            
+        } = req.body;
+        const post = await Contact.create({
+            
+        })
+            
+        res.status(201).json({
+            success: true,
+            post,
+        });
+            
+    }
+            
+);
+
+exports.getPost = catchAsyncError(
+    async (req, res, next) => {
+        const post = await Contact.findById(req.params.id);
+        if (!post) {
+            return next(
+                new ErrorHandler(`Post does not exist with Id: ${req.params.id}`, 404)
+            );
+        }
+        res.status(200).json({
+            success: true,
+            post,
+        });
+    }
+);
+
+exports.updatePost = catchAsyncError(
+    async (req, res, next) => {
+        const post = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!post) {
+            return next(
+                new ErrorHandler(`Post does not exist with Id: ${req.params.id}`, 404)
+            );
+        }
+        res.status(200).json({
+            success: true,
+            post,
+        });
+    }
+)
+
+exports.deletePost = catchAsyncError(
+    async (req, res, next) => {
+        const post = await Contact.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return next(
+                new ErrorHandler(`Post does not exist with Id: ${req.params.id}`, 404)
+            );
+        }
+        res.status(200).json({
+            success: true,
+            post,
+        });
+    }
+)
+
